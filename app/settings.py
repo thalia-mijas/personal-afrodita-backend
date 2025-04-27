da475079a9518f75208eed0d0cf2ad94a32d0023
Thalia Mijas <tlmijas@outlook.com>
2025-04-27 17:37:16 -0500

    :refactor: Update deploy settings

--- app/settings.py
+++ app/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "afroditaapi.urls"
+ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "afroditaapi.wsgi.application"
+WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
