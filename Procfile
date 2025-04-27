3ecebd39d389448fd080410b399bd3e86da4aee5
Thalia Mijas <tlmijas@outlook.com>
2025-04-27 17:49:12 -0500

    :refactor: Update deploy settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'afrodita.wsgi'
--- afroditaapi/settings.py
+++ afroditaapi/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "app.urls"
+ROOT_URLCONF = "afroditaapi.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.wsgi.application"
+WSGI_APPLICATION = "afroditaapi.wsgi.application"
 
 
 # Database
